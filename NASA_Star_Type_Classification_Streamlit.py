7c2e052ab880fc95e9e36df5f50e7782667cc0f4
Shuchita Mishra <shuchitamishra@Shuchitas-MacBook-Air.local>
2022-04-01 08:40:57 -0400

    Updating execution path

--- NASA_Star_Type_Classification_Streamlit.py
+++ NASA_Star_Type_Classification_Streamlit.py
@@ -16,7 +16,8 @@ from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 
-stars = pd.read_csv('/Users/shuchitamishra/Desktop/Jobs/OA/NASA-Star-Type-Classification/FinalProcessed.csv')
+
+stars_ohe = pd.read_csv('/Users/shuchitamishra/Desktop/Jobs/OA/NASA-Star-Type-Classification/FinalProcessed.csv')
 
 scaler = StandardScaler()
 
@@ -24,7 +25,7 @@ stars_ohe_scaled = scaler.fit(stars_ohe.drop('Type', axis = 1))
 stars_ohe_scaled = scaler.transform(stars_ohe.drop('Type', axis = 1))
 
 X = pd.DataFrame(stars_ohe_scaled, columns = stars_ohe.columns[:-1])
-Y = stars_ohe_scaled['Type']
+Y = stars_ohe['Type']
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 42, test_size = 0.33)
 
@@ -51,17 +52,17 @@ predict_knn_5 = model_knn_5.predict(X_test)
 print(classification_report(Y_test,predict_knn_1))
 print(classification_report(Y_test,predict_knn_5))
 
-error_rate= []
-for i in range(1,40):
-    model_knn = KNeighborsClassifier(n_neighbors = i)
-    model_knn.fit(X_train,Y_train)
-    predict_knn_i = model_knn.predict(X_test)
-    error_rate.append(np.mean(predict_knn_i != Y_test))
-plt.figure(figsize = (10,6))
-plt.plot(range(1,40),error_rate,color = 'blue',linestyle = '--',marker = 'o',markerfacecolor='red',markersize = 3)
-plt.title('Error Rate vs K')
-plt.xlabel('K')
-plt.ylabel('Error Rate')
+#error_rate= []
+#for i in range(1,40):
+#    model_knn = KNeighborsClassifier(n_neighbors = i)
+#    model_knn.fit(X_train,Y_train)
+#    predict_knn_i = model_knn.predict(X_test)
+#    error_rate.append(np.mean(predict_knn_i != Y_test))
+#plt.figure(figsize = (10,6))
+#plt.plot(range(1,40),error_rate,color = 'blue',linestyle = '--',marker = 'o',markerfacecolor='red',markersize = 3)
+#plt.title('Error Rate vs K')
+#plt.xlabel('K')
+#plt.ylabel('Error Rate')
 
 print(model_knn_5.score(X_test, Y_test))
 
@@ -72,20 +73,20 @@ avg_cv_scores = np.mean(cv_scores)
 print(avg_cv_scores)
 
 # choose k between 1 to 40
-k_range = range(1, 40)
-k_scores = []
+#k_range = range(1, 40)
+#k_scores = []
 # use iteration to caclulator different k in models, then return the average accuracy based on the cross validation
-for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X, Y, cv=5, scoring='accuracy')
-    k_scores.append(scores.mean())
+#for k in k_range:
+#    knn = KNeighborsClassifier(n_neighbors=k)
+#    scores = cross_val_score(knn, X, Y, cv=5, scoring='accuracy')
+#    k_scores.append(scores.mean())
 
 # plot to see clearly
-plt.figure(figsize = (10,6))
-plt.plot(k_range, k_scores, color = 'blue',linestyle = '--',marker = 'o',markerfacecolor='red',markersize = 3)
-plt.xlabel('Value of K for KNN')
-plt.ylabel('Cross-Validated Accuracy')
-plt.show()
+#plt.figure(figsize = (10,6))
+#plt.plot(k_range, k_scores, color = 'blue',linestyle = '--',marker = 'o',markerfacecolor='red',markersize = 3)
+#plt.xlabel('Value of K for KNN')
+#plt.ylabel('Cross-Validated Accuracy')
+#plt.show()
 
 params={'n_neighbors': range(1,40)}
 params
@@ -111,9 +112,6 @@ L_ip = st.slider('Luminosity:', -0.6, 4.3)
 R_ip = st.slider('Relative radius',-0.4, 3.5)
 A_M_ip = st.slider('Absolute Magnitude:', -1.5, 3.5)
 Type_ip = st.radio("Type:", ("-0.47" ,"2.12"))
-genre = st.radio(
-     "What's your favorite movie genre",
-     ('Comedy', 'Drama', 'Documentary'))
 Color_Blue_White_ip = st.radio("Is the color blue-white?", ["0", "1"])
 Color_Orange_ip = st.radio("Is the color orange?", ["0", "1"])
 Color_Red_ip = st.radio("Is the color red?", ["0", "1"])
@@ -124,6 +122,7 @@ Class_F_ip = st.radio("Is the spectral class F?", ["0", "1"])
 Class_G_ip = st.radio("Is the spectral class G?", ["0", "1"])
 Class_K_ip = st.radio("Is the spectral class K?", ["0", "1"])
 Class_M_ip = st.radio("Is the spectral class M?", ["0", "1"])
+Class_O_ip = st.radio("Is the spectral class O?", ["0", "1"])
 
 data = {'Temperature': Temperature_ip,
         'Luminosity': L_ip,
@@ -140,11 +139,11 @@ data = {'Temperature': Temperature_ip,
         'Class_G' : Class_G_ip,
         'Class_K' : Class_K_ip,
         'Class_M' : Class_M_ip,
+        'Class_O' : Class_O_ip
         }
 
 features = pd.DataFrame(data, index=[0])
-features.shape
-#X_train.shape
+print(features.shape, X_train.shape)
 
 pred_proba = model_knn_5.predict_proba(features)
 #or
